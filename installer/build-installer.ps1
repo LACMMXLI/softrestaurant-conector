f9f2aec33a57a6d222c7e9cd5313cd7af7f8c232
LACMMXLI <alonzocardona123@gmail.com>
2026-08-29 19:17:17 -0700

    feat: add PowerShell script to automate build and installer creation for SoftRestaurant-Sync-Agent

--- installer/build-installer.ps1
+++ installer/build-installer.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param(
-    [string] $Version = '1.1.1',
+    [string] $Version = '1.1.2',
     [string] $ApiUrl = 'https://softrestaurant-api.fatboymexicali.com',
     [switch] $TestBuild
 )
